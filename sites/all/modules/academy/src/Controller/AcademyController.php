10b8441a9234d4ac75e7e60f962555300ca3e5fa
Jeynova <novice.jeremy@gmail.com>
2019-09-23 14:56:41 +0200

    Init

---
+++ sites/all/modules/academy/src/Controller/AcademyController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\academy\Controller;
+
+
+class AcademyController
+{
+  public function hello()
+  {
+    return array(
+      '#title' => 'Mon module',
+      '#description' => 'Ceci est mon module'
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
